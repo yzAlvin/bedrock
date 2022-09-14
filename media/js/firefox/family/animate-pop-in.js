97ab60ac3718c903dbd2aa3c324ababa3db82f2f
maureenlholland <maureen.holland11@gmail.com>
2022-09-14 11:51:52 +0100

    Add basic pop-in

---
+++ media/js/firefox/family/animate-pop-in.js
@@ -0,0 +1,52 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+/* eslint-disable */
+// copy/paste
+
+(function () {
+    'use strict';
+
+    // Function to test if Intersection Observer is supported
+    var supportsInsersectionObserver = (function () {
+        return (
+            'IntersectionObserver' in window &&
+            'IntersectionObserverEntry' in window &&
+            'intersectionRatio' in window.IntersectionObserverEntry.prototype
+        );
+    })();
+
+    // Check for support
+    if (
+        supportsInsersectionObserver &&
+        window.NodeList &&
+        NodeList.prototype.forEach
+    ) {
+        setTimeout(function () {
+            var observer = new IntersectionObserver(
+                function (entries) {
+                    entries.forEach(function (entry) {
+                        // trigger animation by adding relevant class with animation styles
+                        if (entry.isIntersecting) {
+                            console.log('intersecting');
+                            entry.target.classList.add('animate-pop-in');
+                            // remove observer after triggering animation
+                            observer.unobserve(entry.target);
+                        }
+                    });
+                },
+                { rootMargin: '-40px' }
+            );
+
+            // add observers
+            document
+                .querySelectorAll('.c-browser:not(.t-color-switch)')
+                .forEach(function (element) {
+                    observer.observe(element);
+                });
+        }, 200);
+    }
+})();
